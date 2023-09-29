5543232b7e5933dff63eb7e18ddc75b655e3d2be
Kaan Alptakin <92170029+kaanalptkn@users.noreply.github.com>
2023-09-29 13:28:10 +0100

    Solved task

---
+++ python_loop.py
@@ -0,0 +1,7 @@
+if __name__ == '__main__':
+    n = int(input())
+    
+    # Loop for non-negative integers less than n
+    for i in range(n):
+    # Print the square of each number
+        print(i * i)
